81195d165dfa290a35734108a2f81a1c45439dd5
Thrishal Yeggoni <thrishalyeggoni@gmail.com>
2026-07-17 15:12:15 +0530

    add everything

---
+++ src/main/java/COM/THRISHAL/AUTOSUGGEST/AutosuggestApplication.java
@@ -0,0 +1,13 @@
+package COM.THRISHAL.AUTOSUGGEST;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutosuggestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutosuggestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/COM/THRISHAL/AUTOSUGGEST/AutosuggestApplicationTests.java
@@ -0,0 +1,13 @@
+package COM.THRISHAL.AUTOSUGGEST;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutosuggestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
